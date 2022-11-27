2749e709807327dd263498c3a12073a751381f8b
renana rimon <renana1414@gmail.com>
2022-11-27 08:46:58 -0800

    mync

--- matala2/mync.c
+++ matala2/mync.c
@@ -1,4 +1,153 @@
+#include <netinet/in.h> //structure for storing address information
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h> //for socket APIs
+#include <sys/types.h>
+
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h> // read(), write(), close()
+
+#define MAX 80
+// #define PORT 8080
+#define SA struct sockaddr
+
+void client_func(int sockfd)
+{
+	char buff[MAX];
+	int n;
+	for (;;) {
+		bzero(buff, sizeof(buff));
+		// printf("Enter the string : ");
+		n = 0;
+		while ((buff[n++] = getchar()) != '\n');
+		write(sockfd, buff, sizeof(buff));
+		
+	}
+}
+
+void server_func(int connfd)
+{
+    char buff[MAX];
+    // infinite loop for chat
+    for (;;)
+    {
+        bzero(buff, MAX);
+
+        // read the message from client and copy it in buffer
+        read(connfd, buff, sizeof(buff));
+        // print buffer which contains the client contents
+        printf("%s\n", buff);
+        bzero(buff, MAX);
+        
+    }
+}
+
 int main(int argc, char const *argv[])
 {
+
+    if (argc != 3)
+    {
+        printf("error");
+        return 1;
+    }
+    if (strcmp(argv[1], "-l") == 0)
+    {
+        int sockfd, connfd, len;
+        struct sockaddr_in servaddr, cli;
+
+        // socket create and verification
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd == -1)
+        {
+            printf("socket creation failed...\n");
+            exit(0);
+        }
+        else
+            printf("Socket successfully created..\n");
+        bzero(&servaddr, sizeof(servaddr));
+
+        // assign IP, PORT
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        servaddr.sin_port = htons(atoi(argv[2]));
+
+        // Binding newly created socket to given IP and verification
+        if ((bind(sockfd, (SA *)&servaddr, sizeof(servaddr))) != 0)
+        {
+            printf("socket bind failed...\n");
+            exit(0);
+        }
+        // else
+        // 	printf("Socket successfully binded..\n");
+
+        // Now server is ready to listen and verification
+        if ((listen(sockfd, 5)) != 0)
+        {
+            printf("Listen failed...\n");
+            exit(0);
+        }
+        // else
+        // 	printf("Server listening..\n");
+        len = sizeof(cli);
+
+        // Accept the data packet from client and verification
+        connfd = accept(sockfd, (SA *)&cli, &len);
+        if (connfd < 0)
+        {
+            printf("server accept failed...\n");
+            exit(0);
+        }
+        server_func(connfd);
+        // After chatting close the socket
+        close(sockfd);
+    }
+    else
+    {
+        int sockfd, connfd;
+        struct sockaddr_in servaddr, cli;
+
+        // socket create and verification
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd == -1)
+        {
+            printf("socket creation failed...\n");
+            exit(0);
+        }
+        else
+            printf("Socket successfully created..\n");
+        bzero(&servaddr, sizeof(servaddr));
+
+        // assign IP, PORT
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_addr.s_addr = inet_addr(argv[1]); // ip
+        servaddr.sin_port = htons(atoi(argv[2]));      // port
+
+        // connect the client socket to server socket
+        if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) != 0)
+        {
+            printf("connection with the server failed...\n");
+            exit(0);
+        }
+        else
+            printf("connected to the server..\n");
+
+        // function for chat
+        client_func(sockfd);
+
+        // close the socket
+        close(sockfd);
+    }
+
     return 0;
 }
